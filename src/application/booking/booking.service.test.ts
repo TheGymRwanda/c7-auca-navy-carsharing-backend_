7d106d0a7f43cfafb21694d881e2695255a51bf7
Varma Cephas <varmac231@gmail.com>
2026-01-20 09:58:00 +0200

    feat: implement unit test for updating booking

---
+++ src/application/booking/booking.service.test.ts
@@ -0,0 +1,48 @@
+import { UserBuilder } from 'src/builders'
+
+import {
+  type BookingRepositoryMock,
+  type DatabaseConnectionMock,
+  mockDatabaseConnection,
+  mockBookingRepository,
+} from '../../mocks'
+import { CarID } from '../car'
+
+import { BookingState } from './booking-state'
+import { BookingBuilder } from './booking.builder'
+import { BookingService } from './booking.service'
+
+describe('BookingService', () => {
+  let bookingService: BookingService
+  let databaseConnectionMock: DatabaseConnectionMock
+  let bookingRepositoryMock: BookingRepositoryMock
+
+  beforeEach(() => {
+    bookingRepositoryMock = mockBookingRepository()
+    databaseConnectionMock = mockDatabaseConnection()
+
+    bookingService = new BookingService(
+      databaseConnectionMock,
+      bookingRepositoryMock,
+    )
+  })
+
+  describe('update', () => {
+    xit('should uupdate a booking', async () => {
+      const renter = new UserBuilder().build()
+      const carId = 4 as CarID
+      const booking = new BookingBuilder()
+        .withCarId(carId)
+        .withState(BookingState.PENDING)
+        .withRenterId(renter.id)
+        .build()
+      const updatedBooking = BookingBuilder.from(booking)
+        .withState(BookingState.ACCEPTED)
+        .build()
+
+      await expect(
+        bookingService.update(updatedBooking, booking.id, renter.id),
+      ).resolves.toEqual(updatedBooking)
+    })
+  })
+})
