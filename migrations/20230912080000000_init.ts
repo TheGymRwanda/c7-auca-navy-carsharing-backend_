d21e85392976b8b5632d19dd66775054756928cc
Varma Cephas <varmac231@gmail.com>
2026-02-02 22:48:51 +0200

    fix: removed changes from previos migration file and generate new one for the bookings table, update package.json file to include migration down script

--- migrations/20230912080000000_init.ts
+++ migrations/20230912080000000_init.ts
@@ -65,46 +65,10 @@ export function up(pgm: MigrationBuilder): void {
       notNull: false,
     },
   })
-
-  pgm.createType('booking_state', [
-    'PICKED_UP',
-    'PENDING',
-    'ACCEPTED',
-    'DECLINED',
-    'RETURNED',
-  ])
-
-  pgm.createTable('bookings', {
-    id: {
-      type: 'serial',
-      primaryKey: true,
-    },
-    car_id: {
-      type: 'integer',
-      references: 'cars',
-    },
-    state: {
-      type: 'booking_state',
-      notNull: true,
-    },
-    renter_id: {
-      type: 'integer',
-      references: 'users',
-    },
-    start_date: {
-      type: 'TIMESTAMP',
-      notNull: true,
-    },
-    end_date: {
-      type: 'TIMESTAMP',
-      notNull: true,
-    },
-  })
 }
 
 export function down(pgm: MigrationBuilder): void {
   pgm.dropTable('cars')
-  pgm.dropTable('bookings')
   pgm.dropTable('car_types')
   pgm.dropTable('users')
 }
